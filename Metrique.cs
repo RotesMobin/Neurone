6a9bac605c0955f58fb1f1661583c655f3fdbc6d
Robin <robinboitemail@gmail.com>
2016-11-29 18:05:07 +0100

    C'est la metrique

---
+++ Metrique.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TypeMetrique{
+	score,tempsSurvie,tempsImo,freqColisionMalus,nbBonusRate
+}
+public class Metrique : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
